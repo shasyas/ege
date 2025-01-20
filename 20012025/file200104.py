54ee01cfa8d838d40c49dac0ae0df68c9ba75cde
shasyas <sashashuygina@gmail.com>
2025-01-20 15:29:48 +0300

    connit

--- 20012025/file200104.py
+++ 20012025/file200104.py
@@ -1,9 +1,18 @@
 f = open('17.txt')
 
-a = [0,0,0,0]
+a = 0
 b = []
-c = []
-d = []
+m = 0
 
 for i in range(5542):
     e = int(f.readline())
+    b.append(e)
+
+for k in range(5541):
+    i = b[k]
+    j = b[k+1]
+    if (i+j)%7 == 0 and (i*j)%15 == 0:
+        a = a + 1
+        m = max(m, i+j)
+
+print(a, m)
