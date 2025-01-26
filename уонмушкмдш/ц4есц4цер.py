aa46800fce506dc1e3e0a553a57fc7faf85e9e44
shasyas <sashashuygina@gmail.com>
2025-01-26 22:57:41 +0300

    connit

--- уонмушкмдш/ц4есц4цер.py
+++ уонмушкмдш/ц4есц4цер.py
@@ -5,8 +5,6 @@ h = 10000
 def zxc(p, t):
     x, y = p
     if t == 0:
-        if (2 < y) or (-2 < x < (-1.6) and -4 < y < -2) or ((3.6) < x < (4.4) and -4 < y < (-3.2)) or ((12.4) < x and (-1.2) < y):
-            return 6
         return (y < -x)
     if y > 1.2*x + 12:
         return 5
@@ -21,35 +19,35 @@ def fye(a, b):
     x1, y1 = a
     x2, y2 = b
 
-    return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+    if ((x1 - x2)**2 + (y1 - y2)**2)**0.5 > 1:
+        return [0,((x1 - x2)**2 + (y1 - y2)**2)**0.5]
+
+    return [1,((x1 - x2)**2 + (y1 - y2)**2)**0.5]
 
 
 for t in range(2):
     fo = open(str(files[t]))
     fo.readline()
-    cs = [[], [], [], [], [], [], []]
+    cs = [[], [], [], [], [], []]
     n = c[t]
 
     for line in fo:
         p = [float(d) for d in line.replace(',', '.').split()]
-        if
-            u = zxc(p, t)
-            cs[u].append(p)
+        cs[zxc(p, t)].append(p)
 
     ms = []
 
     for i in range(n):
-        dm = 0
-        k = [0,0]
+        dm = 1
 
         for p in cs[i]:
-            d = sum(fye(p, j) for j in cs[i])
-            if d > dm:
+            d = sum(fye(p, j)[1] for j in cs[i])
+            g = sum(fye(p, j)[0] for j in cs[i])
+            if d > dm and g > 0:
                 k = p
                 dm = d
 
         ms.append(k)
-
 
     px = sum(j[0] for j in ms)/c[t] * h
     py = sum(j[1] for j in ms)/c[t] * h
