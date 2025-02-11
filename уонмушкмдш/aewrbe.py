a5a4991e5fb294763581b7c2420ebcd9194e9f39
shasyas <sashashuygina@gmail.com>
2025-02-11 03:09:17 +0300

    connit

---
+++ уонмушкмдш/aewrbe.py
@@ -0,0 +1,9 @@
+from beautifulsoup4 import BeautifulSoup
+
+soup = BeautifulSoup('<html><head><title>Заголовок страницы</title></head><body></body></html>', 'html.parser')
+
+# Получаем тег body и добавляем в него параграф
+soup.body.append(soup.new_tag('p', id='intro'))
+soup.body.p.string = 'Добро пожаловать на мою страницу!'
+
+print(soup.prettify())
